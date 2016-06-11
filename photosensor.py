ca7aeca28a0567afd65000a14ac3d86242e6feb5
chris <chris@c-mart.in>
2016-06-11 13:36:40 -0700

    working brightness control from photosensor input

--- photosensor.py
+++ photosensor.py
@@ -1,3 +1,13 @@
+"""
+If buttons are pressed on micro:bit, enter brightness menu loop
+Displays current brightness, push buttons to go up and down
+Waits 3 seconds, then saves current value to flash
+How to influence neighboring values? Display brightness should monotonically increase with LDR value
+If a user input is saved, any resulting non-monotonic parts of the curve can be "flattened"
+
+Also, set a threshold for changing brightness. Check more frequently but only change it if LDR value changes by more than a threshold.
+"""
+
 from microbit import *
 
 bright_lookup = dict()
@@ -9,29 +19,51 @@ def round_down_50(x):
     # Round down to nearest multiple of 50; maps LDR values to brightness levels
     return x - (x % 50)
 
-def push_button(pin_num, count):
+
+def push_button(pin_obj, count):
     # Cycles pin_num count number of times, to push corresponding buttons
-    pin_obj = eval("pin{0}".format(pin_num))
+    # Monitor doesn't recognize all presses if sleep values are decreased
     for i in range(count):
         pin_obj.write_digital(1)
-        sleep(50)
+        sleep(65)
         pin_obj.write_digital(0)
-        sleep(200)
+        sleep(65)
 
 def change_brightness(increment):
     # Changes monitor brightness by increment steps, by pushing buttons
     # Push 'brightness up' button to enter brightness menu
-    push_button(12, 1)
+    push_button(pin12, 1)
+    sleep(500)
     # Going down
     if increment < 0:
-        push_button(8, abs(increment))
+        push_button(pin8, abs(increment))
     # Going up
     else:
-        push_button(12, increment)
+        push_button(pin12, increment)
+    sleep(200)  # Remove this to make things faster
     # Push 'menu' button to exit brightness menu
-    push_button(16, 1)
+    push_button(pin16, 1)
 
-cur_bright = 50
+def reset_to_0():
+    # Resets brightness to 0 when program starts
+    display.scroll("CALIBRATING", delay=80, wait=False)
+    pin12.write_digital(1)
+    sleep(100)
+    pin12.write_digital(0)
+    sleep(100)
+    pin8.write_digital(1)
+    sleep(6500)
+    pin8.write_digital(0)
+    sleep(100)
+    pin16.write_digital(1)
+    sleep(100)
+    pin16.write_digital(0)
+    sleep(500)
+
+cur_bright = 0
+
+# Main program execution
+reset_to_0()
 
 while True:
     ldr_val = pin0.read_analog()
